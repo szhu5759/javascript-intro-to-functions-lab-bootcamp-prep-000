3f041dbdd4e46eef0ffc0061370cdd3ec71633a4
szhu5759 <szhu5759@gmail.com>
2019-03-26 04:31:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,5 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-
 function sayHiToGrandma(string) {
-  return "I love you, Grandma."
-}
+  return string.toLowerCase()
